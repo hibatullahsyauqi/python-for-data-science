cd68b013b7c2a760b18495debb94754fffdc0d7c
iqqqqquays <hibatullahsyauqiattaqi@gmail.com>
2024-09-19 10:41:30 +0700

    weight converter

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-'''
+"""
 print('Patient\'s data')
 name=input('Name: ')
 age=int(input('Age: '))
 print('New patient named', name,'is', age,'years old')
-'''
+"""
 
-'''
+"""
 print('Someone\'s favorite color')
 name=input('Enter your name: ')
 color=input('Enter your favorite color: ')
 print(name, 'likes', color)
-'''
+"""
 
+'''
 birth_year=int(input('Birth year: '))
 age=2024-birth_year
 print('You are', age, 'years old')
+'''
+
+print('Weight Converter lbs to kgs')
+weight_lbs=int(input('Insert your weight in lbs: '))
+weight_kgs=int(weight_lbs*2.20462)
+print('You weigh', weight_kgs, 'kgs')
