09508c1655b69d62a2dc6066a53047bd3685c5b1
iqqqqquays <hibatullahsyauqiattaqi@gmail.com>
2024-09-19 09:30:29 +0700

    birth year

--- app.py
+++ app.py
@@ -5,7 +5,13 @@ age=int(input('Age: '))
 print('New patient named', name,'is', age,'years old')
 '''
 
+'''
 print('Someone\'s favorite color')
 name=input('Enter your name: ')
 color=input('Enter your favorite color: ')
 print(name, 'likes', color)
+'''
+
+birth_year=int(input('Birth year: '))
+age=2024-birth_year
+print('You are', age, 'years old')
