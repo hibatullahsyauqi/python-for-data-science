6fd2703e91ff8a3dae29a9625cb846a60ddf8f15
iqqqqquays <hibatullahsyauqiattaqi@gmail.com>
2024-09-19 09:14:40 +0700

    variables_colors

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+'''
 print('Patient\'s data')
 name=input('Name: ')
 age=int(input('Age: '))
 print('New patient named', name,'is', age,'years old')
+'''
+
+print('Someone\'s favorite color')
+name=input('Enter your name: ')
+color=input('Enter your favorite color: ')
+print(name, 'likes', color)
